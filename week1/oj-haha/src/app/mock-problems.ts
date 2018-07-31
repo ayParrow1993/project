438e384386d19e46450d197a2f412ff036d2e00b
ayParrow1993 <yl4752@nyu.edu>
2018-07-31 13:53:40 -0400

    hw1

---
+++ week1/oj-haha/src/app/mock-problems.ts
@@ -0,0 +1,32 @@
+import {Problem} from './models/problem.model';
+
+export const PROBLEMS: Problem[] = [
+{
+"id":1,
+"name":"Two Sum",
+"desc":"Given an array of integers, find two numbers such that they add up to",
+"difficulty":"easy"
+},
+{
+"id":2,
+"name":"3Sum",
+"desc":"Given an array S of n integers, are there elements a, b, c in S such that a +.",
+"difficulty":"medium"
+},
+{
+"id":3,
+"name":"4Sum",
+"desc":"Given an array S of n integers, are there elements a, b, c, and d in.",
+"difficulty":"medium"
+},
+{
+"id":4,
+"name":"Triangle Count",
+"desc":"Given an array of integers, how many three numbers can be found in?",
+"difficulty":"hard"},
+{
+"id":5,
+"name":"Sliding Window Maximum",
+"desc":"Given an array of n integer with duplicate number, and a moving.",
+"difficulty":"super"
+}];
